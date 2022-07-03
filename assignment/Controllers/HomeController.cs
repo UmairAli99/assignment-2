894f9f3c2bda166e5cd5df4d152a452e51b0c828
umairbaryar99@outlook.com <umairbaryar99@outlook.com>
2022-07-03 14:59:44 +0500

    Quiz 2

--- assignment/Controllers/HomeController.cs
+++ assignment/Controllers/HomeController.cs
@@ -3,9 +3,18 @@
 namespace assignment.Controllers
 {
     public class HomeController : Controller
-    {
-        public IActionResult Index()
+    { 
+        public IActionResult Index(string fname,string lname,string phone,string mail,string gender,string dept)
         {
+            if(fname=="Umair" && lname == "Ali" && phone == "+923082289363" && mail == "19101001-115@uskt.edu.pk" &&
+                gender == "male" || gender == "female" && dept == "sales" || dept == "it" )
+            {
+                ViewBag.result = "Data submitted Successfully...";
+            }
+            else
+            {
+                ViewBag.result = "Please Enter right data...";
+            }
             return View();
         }
     }
